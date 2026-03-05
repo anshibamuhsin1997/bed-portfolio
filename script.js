6d48a6f69e367e613aee8b5db39496340af9ba88
Muhammed Muhsin <muhsindbitlab@gmail.com>
2026-03-06 02:00:33 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+
+const menuBtn = document.getElementById("menuBtn");
+const navMenu = document.getElementById("navMenu");
+
+menuBtn.onclick = function(){
+
+if(navMenu.style.display === "flex"){
+navMenu.style.display="none";
+}
+else{
+navMenu.style.display="flex";
+}
+
+};
+
+
+const themeToggle = document.getElementById("themeToggle");
+const themeIcon = document.getElementById("themeIcon");
+const themeText = document.getElementById("themeText");
+
+themeToggle.onclick = () => {
+
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+
+themeIcon.innerHTML="☀️";
+themeText.innerHTML="Light";
+
+}else{
+
+themeIcon.innerHTML="🌙";
+themeText.innerHTML="Dark";
+
+}
+
+};
+
+
+ScrollReveal().reveal('.hero-content',{
+delay:200,
+distance:'50px',
+origin:'bottom'
+});
+
+ScrollReveal().reveal('.card',{
+interval:200,
+distance:'40px',
+origin:'bottom'
+});
+
+ScrollReveal().reveal('.timeline-item',{
+delay:200,
+origin:'left'
+});
+
+ScrollReveal().reveal('.gallery img',{
+interval:200,
+scale:0.9
+});
